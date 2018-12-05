38c1c526d8836a64e96d3dac75abbdd1352229ca
DanerSound <andre.barreto28@gmail.com>
2018-12-05 19:59:59 +0100

    wrapped frame tools into private methods

--- MoveTheCircle/src/main/java/TantoPer/MoveTheCircle/Frame.java
+++ MoveTheCircle/src/main/java/TantoPer/MoveTheCircle/Frame.java
@@ -1,13 +1,11 @@
 package TantoPer.MoveTheCircle;
 
-
 import java.awt.Container;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
 
 public class Frame {
 	private JLabel counter;
@@ -19,34 +17,44 @@ public class Frame {
 		frame.setSize(500, 400);
 		Container myContainer = frame.getContentPane();
 		myContainer.setLayout(null);
-		
 		createButton(frame, mtc);
-		
-		JLabel pointString = new JLabel();
-		pointString.setText("Points:");
-		pointString.setBounds(100,300,100,50);
-		frame.add(pointString);
-		
-		counter = new JLabel();
-		counter.setText("0");
-		counter.setBounds(200,300,100,50);
-		frame.add(counter);
-		
-		Icon image = new ImageIcon(getClass().getResource("circle.png"));
-		img = new JLabel(image); 
-		img.setBounds(100,150, 50, 50);
-		img.setVisible(true);
-		frame.add(img).isVisible();
-				
+		createPointString();
+		createLabelCounter();
+		imagineLoader("circle.png");
+
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
 
 	private void createButton(JFrame frame, MTC mtc) {
 		JButton button = new JButton("click me!");
-		button.setBounds(200,20,100,50);
+		button.setBounds(200, 20, 100, 50);
 		button.addMouseListener(new MouseEvent(mtc));
-		frame.add(button);		
+		frame.add(button);
+	}
+
+	private void createLabelCounter() {
+		counter = new JLabel();
+		counter.setText("0");
+		counter.setBounds(200, 300, 100, 50);
+		frame.add(counter);
+	}
+
+	private void imagineLoader(String fileName) {
+		Icon image = new ImageIcon(getClass().getResource(fileName));
+		img = new JLabel(image);
+		img.setBounds(100, 150, 50, 50);
+		img.setVisible(true);
+		frame.add(img).isVisible();
+
+	}
+
+	private void createPointString() {
+		JLabel pointString = new JLabel();
+		pointString.setText("Points:");
+		pointString.setBounds(100, 300, 100, 50);
+		frame.add(pointString);
+
 	}
 
 	public void updateCounter(String text) {
